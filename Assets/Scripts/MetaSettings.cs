a925f3febcbef187bccc6d936a59d6fb79f42abf
nemlyc_desktop <cf.m1t0k@gmail.com>
2020-01-16 01:45:54 +0900

    add: IPアドレスとポート番号を設定する画面を作成

---
+++ Assets/Scripts/MetaSettings.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MetaSettings : MonoBehaviour
+{
+    public static string AccessIPAddress;
+    public static string AccessPort;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public static string getIPAddress() {
+        return AccessIPAddress;
+    }
+    public static string getPortNumber() {
+        return AccessPort;
+    }
+
+}
---
+++ Assets/Scripts/ValueChecker.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class ValueChecker : MonoBehaviour
+{
+    [SerializeField]
+    private TMP_InputField ip_inputField;
+    [SerializeField]
+    private TMP_InputField port_inputField;
+
+    [SerializeField]
+    private TMP_Text ipAddress;
+    [SerializeField]
+    private TMP_Text portNumber;
+    [SerializeField]
+    private TMP_Text error;
+
+    private string ControllerScene = "Controller";
+    private string ErrorMessage = "IPAddress or PortNumber is empty !!";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ip_inputField = ip_inputField.GetComponent<TMP_InputField>();
+        port_inputField = port_inputField.GetComponent<TMP_InputField>();
+
+        ipAddress = ipAddress.GetComponent<TMP_Text>();
+        portNumber = portNumber.GetComponent<TMP_Text>();
+        error = error.GetComponent<TMP_Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void InputIPAddress() {
+        ipAddress.text = ip_inputField.text;
+        MetaSettings.AccessIPAddress = ipAddress.text;
+    }
+    public void InputPortNumber() {
+        portNumber.text = port_inputField.text;
+        MetaSettings.AccessPort = portNumber.text;
+    }
+    public void OnClick() {
+        if (MetaSettings.AccessIPAddress != null && MetaSettings.AccessPort != null) {
+            SceneManager.LoadScene(ControllerScene);
+        } else {
+            error.text = ErrorMessage;
+        }
+    }
+}
